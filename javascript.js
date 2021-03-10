43550faca804c90205e6f3c92d29248485bfc6f6
unknown <jayshrijs98@gmail.com>
2021-03-10 10:11:40 +0530

    upto Javascript

---
+++ javascript.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JS</title>
+	<style>
+		p{
+			background-color: pink;
+			text-align: center;
+
+		}
+	</style>
+</head>
+<body>
+<b><p id="id"></p></b>
+	<script type="text/javascript">
+		document.getElementById('id').innerHTML ="JAVASCRIPT";
+		function myfunction()
+	{
+		document.getElementById('id').style.fontSize='100px';
+	}
+	</script>
+<ody>
+</html>
